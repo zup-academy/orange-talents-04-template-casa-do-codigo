cbdb90b5f15443d6abf7e6f4fdb05bc7361569e6
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-04-20 17:59:43 -0300

    adicionado getters para Livro e Categoria

--- src/main/java/br/com/zupacademy/ggwadera/casadocodigo/categoria/Categoria.java
+++ src/main/java/br/com/zupacademy/ggwadera/casadocodigo/categoria/Categoria.java
@@ -22,4 +22,12 @@ public class Categoria {
     public Categoria(String nome) {
         this.nome = nome;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
--- src/main/java/br/com/zupacademy/ggwadera/casadocodigo/livro/Livro.java
+++ src/main/java/br/com/zupacademy/ggwadera/casadocodigo/livro/Livro.java
@@ -67,6 +67,38 @@ public class Livro {
         return titulo;
     }
 
+    public String getResumo() {
+        return resumo;
+    }
+
+    public String getSumario() {
+        return sumario;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public Integer getNumeroDePaginas() {
+        return numeroDePaginas;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
     public static class Builder {
         private String titulo;
         private String resumo;
