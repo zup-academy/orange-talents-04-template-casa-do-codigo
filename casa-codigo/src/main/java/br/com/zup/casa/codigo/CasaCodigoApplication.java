1c45641f218c237680dcdf33e0d38a6a7e071c66
Adrici <cirellia5@gmail.com>
2021-04-23 12:31:20 -0300

    cadastro-novo-autor

---
+++ casa-codigo/src/main/java/br/com/zup/casa/codigo/CasaCodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.casa.codigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaCodigoApplication.class, args);
+	}
+
+}
