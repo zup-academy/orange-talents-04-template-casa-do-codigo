12c7cf2ed73103a0e120288e7c537f1ebe2c2361
Guilherme de Mira <guilhermemirar@gmail.com>
2021-04-27 10:42:46 -0300

    setup

---
+++ src/test/java/com/zupacademy/demira/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zupacademy.demira.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
